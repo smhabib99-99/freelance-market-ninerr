e37b1305262b6ba46fb77e1b3147457b60ed4adf
smhabib99-99 <habibaust34@gmail.com>
2023-11-07 17:25:04 +0600

    basic client side project setup is successful

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwu6LiY-kYmBQUBsxiTe4T27bCtl1pl2I",
+  authDomain: "ninerr-client.firebaseapp.com",
+  projectId: "ninerr-client",
+  storageBucket: "ninerr-client.appspot.com",
+  messagingSenderId: "530581196352",
+  appId: "1:530581196352:web:94a22747fa2ca77d3c19a5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
