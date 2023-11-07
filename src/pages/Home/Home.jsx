1c957e895de4acd1ddb902f812867fcfd25bb227
smhabib99-99 <habibaust34@gmail.com>
2023-11-08 03:56:50 +0600

    Tab Item structure creation successful

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../../components/Banner/Banner";
+import TabCategory from "../TabCategory/TabCategory";
 
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
             {/* <h2 className="text-5xl">This is Home</h2> */}
             <Banner></Banner>
+            <TabCategory></TabCategory>
         </div>
     );
 };
---
+++ src/pages/TabCategory/TabCategory.jsx
@@ -0,0 +1,15 @@
+
+
+const TabCategory = () => {
+    return (
+        <div className="">
+            <div className="tabs text-3xl justify-center">
+                <a className="tab text-3xl tab-lifted">Web Development</a>
+                <a className="tab tab-lifted text-3xl tab-active">Digital Marketing</a>
+                <a className="tab text-3xl tab-lifted">Graphic Design</a>
+            </div>
+        </div>
+    );
+};
+
+export default TabCategory;
