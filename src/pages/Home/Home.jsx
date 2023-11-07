32ff26e7592a3f7407adb54acaf1ef6377d76268
smhabib99-99 <habibaust34@gmail.com>
2023-11-07 22:41:00 +0600

    Banner Carousel Implementation is successful

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../../components/Banner/Banner";
 
 
 const Home = () => {
     return (
         <div>
             <h2 className="text-5xl">This is Home</h2>
+            <Banner></Banner>
         </div>
     );
 };
