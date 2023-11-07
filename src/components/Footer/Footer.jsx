fb18457a2994f6b5811945193b2526490fde646e
smhabib99-99 <habibaust34@gmail.com>
2023-11-08 04:17:43 +0600

    ninerr client side design part successful

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <div>
+        <div className="mt-8">
             <footer className="footer p-10 bg-base-200 text-base-content">
                 <aside>
                     <img className="h-20" src="/public/ninerr2.png" alt="" />
--- src/pages/TabCategory/TabCategory.jsx
+++ src/pages/TabCategory/TabCategory.jsx
@@ -2,8 +2,8 @@
 
 const TabCategory = () => {
     return (
-        <div className="">
-            <div className="tabs text-3xl justify-center">
+        <div className="space-y-8">
+            <div className="tabs text-3xl space-y-8 justify-center">
                 <a className="tab text-3xl tab-lifted">Web Development</a>
                 <a className="tab tab-lifted text-3xl tab-active">Digital Marketing</a>
                 <a className="tab text-3xl tab-lifted">Graphic Design</a>
