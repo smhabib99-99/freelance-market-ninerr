587db3352974b02cb6458b7d69da90b2a7ea12ab
smhabib99-99 <habibaust34@gmail.com>
2023-11-08 03:59:30 +0600

    ErrorPage check functionallity is successful

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -39,6 +39,7 @@ const Navbar = () => {
     <li><NavLink to='/jobDetail'>Job Detail</NavLink></li>
     <li><NavLink to='/register'>Register</NavLink></li>
     <li><NavLink to='/login'>Login</NavLink></li>
+    <li><NavLink to='/service'>Service</NavLink></li>
 
 </>
 
