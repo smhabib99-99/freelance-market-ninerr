0f3495480ebf866c3aa26f6e81cf5ab394d95824
smhabib99-99 <habibaust34@gmail.com>
2023-11-09 14:04:36 +0600

    Add jobs page client side data get successful

--- src/pages/AddJobs/AddJobs.jsx
+++ src/pages/AddJobs/AddJobs.jsx
@@ -18,35 +18,9 @@ const AddJobs = () => {
         const maxPrice = form.maxPrice.value;
         const image = form.image.value;
 
-        const newProduct = { category, title, minPrice, maxPrice, description, email, image }
-        console.log(newProduct);
+        const newJobs = { category, title, minPrice, maxPrice, description, email, image }
+        console.log(newJobs);
 
-
-
-        // fetch('https://brand-shop-server-6zamcshnh-habibs-projects-11338489.vercel.app/product', {
-        //     method: 'POST',
-        //     headers: {
-        //         'content-type': 'application/json'
-        //     },
-        //     body: JSON.stringify(newProduct)
-        // })
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         console.log(data);
-
-                // if (data.insertedId) {
-                //     Swal.fire({
-                //         title: 'Success!',
-                //         text: 'Product Added Successfully.',
-                //         icon: 'success',
-                //         confirmButtonText: 'Cool'
-                //     })
-                // }
-
-                // event.target.reset();
-
-
-            // })
 
     }
 
@@ -54,7 +28,7 @@ const AddJobs = () => {
     return (
         <div>
             <form onSubmit={handleAddJobs} className="mx-5 my-8 p-10">
-                <h2 className="text-3xl text-center font-extrabold bg-slate-700 py-5 text-white">Please input the form to add product.</h2>
+                <h2 className="text-3xl text-center font-extrabold rounded-md bg-amber-400 py-5 text-white">Please input the form to add product.</h2>
                 {/* Category and title */}
                 <div className="md:flex ">
                     <div className="form-control w-1/2">
@@ -124,7 +98,7 @@ const AddJobs = () => {
                         </label>
                     </div>
                 </div>
-                <button type="submit" value="Add Product" className="btn bg-gray-900 text-2xl text-lime-500 btn-block">Add Job</button>
+                <button type="submit" value="Add Product" className="btn bg-amber-400 text-2xl text-blue-500 btn-block">Add Jobs</button>
             </form>
 
         </div >
